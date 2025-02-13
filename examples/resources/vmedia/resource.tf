f6b2b724c9bd438f19531914b03488677c5c6952
zamojskia <andrzej.zamojski@fujitsu.com>
2025-02-13 10:19:46 +0100

    Fix virtual media test scenarios to cover more supported interfaces and make it easier to debugging

--- examples/resources/vmedia/resource.tf
+++ examples/resources/vmedia/resource.tf
@@ -24,6 +24,6 @@ resource "irmc-redfish_virtual_media" "vm" {
     ssl_insecure = each.value.ssl_insecure
   }
 
-  image                  = "http://10.172.181.125:8006/gauge/vmedia/Cd!123.iso"
-  transfer_protocol_type = "HTTP"
+  image                  = "10.172.181.125/gauge/vmedia/Cd!123.iso"
+  transfer_protocol_type = "HTTPS"
 }
